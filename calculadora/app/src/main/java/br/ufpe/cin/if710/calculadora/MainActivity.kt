9f1ff42d337c8b2cdffa2b76bf87734c307daf4e
Divino G. de Menezes Neto <dgmneto@gmail.com>
2018-09-05 00:01:32 -0300

    Implemented calculator

--- calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
@@ -2,12 +2,73 @@ package br.ufpe.cin.if710.calculadora
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var stringBuilder = StringBuilder()
+        var shouldClear = true
+
+        fun appendText(text: CharSequence) {
+            if (shouldClear) {
+                text_calc.setText(text)
+                shouldClear = false
+            } else text_calc.append(text)
+            stringBuilder.append(text)
+        }
+        fun appendOperator(text: CharSequence, shouldPrint: Boolean) {
+            if(shouldPrint && shouldClear) {
+                text_calc.setText(text)
+                shouldClear = false
+            } else {
+                shouldClear = true
+            }
+            stringBuilder.append(text)
+        }
+
+        fun appendOperator(text: CharSequence) {
+            appendOperator(text, false)
+        }
+
         setContentView(R.layout.activity_main)
+        btn_0.setOnClickListener { appendText("0") }
+        btn_1.setOnClickListener { appendText("1") }
+        btn_2.setOnClickListener { appendText("2") }
+        btn_3.setOnClickListener { appendText("3") }
+        btn_4.setOnClickListener { appendText("4") }
+        btn_5.setOnClickListener { appendText("5") }
+        btn_6.setOnClickListener { appendText("6") }
+        btn_7.setOnClickListener { appendText("7") }
+        btn_8.setOnClickListener { appendText("8") }
+        btn_9.setOnClickListener { appendText("9") }
+        btn_Dot.setOnClickListener {appendText(".") }
+        btn_LParen.setOnClickListener {
+            stringBuilder.append("(")
+            shouldClear = true
+        }
+        btn_RParen.setOnClickListener { appendOperator(")") }
+        btn_Power.setOnClickListener { appendOperator("^") }
+        btn_Add.setOnClickListener { appendOperator("+") }
+        btn_Subtract.setOnClickListener { appendOperator("-", true) }
+        btn_Multiply.setOnClickListener { appendOperator("*") }
+        btn_Divide.setOnClickListener { appendOperator("/") }
+        btn_Equal.setOnClickListener {
+            try {
+                var result = eval(stringBuilder.toString())
+                stringBuilder.setLength(0)
+                text_info.text = result.toString()
+                shouldClear = true
+            } catch (ex: RuntimeException) {
+                Toast.makeText(applicationContext, ex.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+        btn_Clear.setOnClickListener {
+            text_calc.setText("0")
+            stringBuilder.setLength(0)
+            shouldClear = true
+        }
     }
 
     //Como usar a função:
